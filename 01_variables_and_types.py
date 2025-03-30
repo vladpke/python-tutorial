c5c7b3613311cb55fd9a67a10092a4188e15508f
Vlad <vladpke@gmail.com>
2025-03-30 14:25:07 +0200

    Finished first module

--- 01_variables_and_types.py
+++ 01_variables_and_types.py
@@ -12,3 +12,16 @@ print("Is student?", is_student)
 # 1. Create a variable for your favorite color
 # 2. Create a variable for your current year in school or work level
 # 3. Print them out nicely
+
+print()
+# 1.
+favorite_color = "petrol blue"
+
+# 2.
+years_of_work = 14
+
+# 3.
+print(f"Vlad's favorite color is {favorite_color}!")
+print(f"I've been working in software for {years_of_work} years")
+
+print()
